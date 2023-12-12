af15a58bb3a5520c229c1a8172ceaa78e4263845
Dima Demakov <d.demakov@gmail.com>
2023-12-12 19:34:49 +0100

    US-1 Init Next.js project with NextUI components

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@nextui-org/button';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p className='w-full justify-center'>
+        <Button>Click me</Button>
+      </p>
+    </main>
+  )
+}
